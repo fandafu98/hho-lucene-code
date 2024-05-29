537fc0726c6fe96672f3dcec65f5d683faedd68a
zhengyankang <792297455@qq.com>
2024-05-30 00:59:45 +0800

    初始化数据优化

--- code-framework/src/main/java/com/hho/framework/runner/InitIndexDataRunner.java
+++ code-framework/src/main/java/com/hho/framework/runner/InitIndexDataRunner.java
@@ -1,12 +1,13 @@
 /**
- * * @author: 郑延康 * @date: 2024-05-28 21:36 */
+ * * @author: 郑延康 * @date: 2024-05-28 21:36
+ */
 package com.hho.framework.runner;
 
 import cn.hutool.core.date.DateField;
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.RandomUtil;
 import com.hho.framework.constant.MokeDataConstant;
 import com.hho.framework.util.LuceneUtil;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.lucene.document.*;
 import org.apache.lucene.index.IndexWriter;
@@ -25,7 +26,7 @@ public class InitIndexDataRunner implements ApplicationRunner {
     // private IndexWriter indexWriter;
 
     @Override
-    public void run(ApplicationArguments args)  {
+    public void run(ApplicationArguments args) {
         IndexWriter indexWriter = LuceneUtil.indexWriter();
         try {
             log.info("=========== 开始初始化lucene内的数据");
@@ -38,25 +39,29 @@ public class InitIndexDataRunner implements ApplicationRunner {
                 document.add(new LongField("id", Long.valueOf(i), Field.Store.YES));
 
                 // 取模，用于生成随机内容的索引
-                int index = i % 5;
+                int index_name = RandomUtil.randomInt(0, 5);
+                int index_type = RandomUtil.randomInt(0, 5);
+                int index_camera = RandomUtil.randomInt(0, 5);
+                int index_screen = RandomUtil.randomInt(0, 5);
+                int status_random = RandomUtil.randomInt(0, 5);
 
                 String title = String.format(MokeDataConstant.TITLE_TEMPLATE,
-                        MokeDataConstant.TITLE_TEMPLATE_NAME[index],
-                        MokeDataConstant.TITLE_TEMPLATE_TYPE[index],
-                        MokeDataConstant.TITLE_TEMPLATE_CAMERA[index],
-                        MokeDataConstant.TITLE_TEMPLATE_SCREEN[index]);
+                        MokeDataConstant.TITLE_TEMPLATE_NAME[index_name],
+                        MokeDataConstant.TITLE_TEMPLATE_TYPE[index_type],
+                        MokeDataConstant.TITLE_TEMPLATE_CAMERA[index_camera],
+                        MokeDataConstant.TITLE_TEMPLATE_SCREEN[index_screen]);
 
                 document.add(new TextField("title", title, Field.Store.YES));
-                document.add(new StringField("status", MokeDataConstant.STATUS_RANDOM[index], Field.Store.YES));
+                document.add(new StringField("status", MokeDataConstant.STATUS_RANDOM[status_random], Field.Store.YES));
                 // 日期从今天开始，2024-05-28 往后偏100天
                 document.add(new LongField("time", DateUtil.offset(today, DateField.DAY_OF_YEAR, i).getTime(), Field.Store.YES));
                 indexWriter.addDocument(document);
             }
             indexWriter.commit();
-        }catch (IOException e){
-            log.error("发生异常",e);
+        } catch (IOException e) {
+            log.error("发生异常", e);
             throw new RuntimeException(e);
-        }finally {
+        } finally {
             try {
                 if (null != indexWriter) {
                     indexWriter.close();
